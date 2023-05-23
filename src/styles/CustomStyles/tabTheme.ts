6ed75372d3839ed4b82685de1579db70a01a9113
IsSabuhi <sabuhi.israfilov2001@gmail.com>
2023-05-24 00:08:48 +0700

    change settings page for jobseeker

--- src/styles/CustomStyles/tabTheme.ts
+++ src/styles/CustomStyles/tabTheme.ts
@@ -20,4 +20,29 @@ const baseStyle = definePartsStyle({
   }
 })
 
-export const tabsTheme = defineMultiStyleConfig({ baseStyle })
+const profileTab = definePartsStyle({
+  root: {
+    background: '#FFF',
+    boxShadow: '0 0px 50px -12px rgba(0, 0, 0, 0.25)',
+    borderRadius: '10px',
+  },
+  tab: {    
+    fontWeight: 700,
+    transition: 'all 200ms ease',
+    textTransform: 'uppercase',
+  },
+  tablist: {
+    alignItems: 'center',
+    gap: '10px',
+    paddingInline: '20px',
+    paddingTop: '15px'
+  }
+})
+
+const variants = {
+  unstyled: baseStyle,
+  profileTab: profileTab,
+}
+
+
+export const tabsTheme = defineMultiStyleConfig({ variants })
